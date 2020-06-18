62b413b62c52bf5e06aa0057d5adf71f6ca6f71b
lynne <lynne@voltanet.io>
2020-06-18 14:06:55 -0400

    fix functions/defines for msgs between IGP and LDP

--- lib/zclient.h
+++ lib/zclient.h
@@ -897,12 +897,14 @@ enum zapi_opaque_registry {
 	LINK_STATE_REQUEST = 1,
 	/* Update containing link-state db info */
 	LINK_STATE_UPDATE = 2,
-	/* Update containing LDP IGP Sync Config info */
-	LDP_IGP_SYNC_IF_CONFIG_UPDATE = 3,
+	/* Request LDP-SYNC state from LDP */
+	LDP_IGP_SYNC_IF_STATE_REQUEST = 3,
 	/* Update containing LDP IGP Sync State info */
 	LDP_IGP_SYNC_IF_STATE_UPDATE = 4,
-	/* Announce the LDP interface to request LDP IGP Sync Config */
-	LDP_IGP_SYNC_IF_ANNOUNCE_UPDATE = 5,
+	/* Announce that LDP is up  */
+	LDP_IGP_SYNC_ANNOUNCE_UPDATE = 5,
+	/* Heartbeat indicating that LDP is running */
+	LDP_IGP_SYNC_HELLO_UPDATE = 6,
 };
 
 /* Send the hello message.
