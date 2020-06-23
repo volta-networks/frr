8a22c9847226d98e81640d2eb8656f60163bc415
Karen Schoener <karen@volta.io>
2020-06-23 10:57:59 -0400

    Remove name from sync-state-req

--- ldpd/ldp_zebra.c
+++ ldpd/ldp_zebra.c
@@ -127,9 +127,6 @@ ldp_zebra_opaque_unregister(void)
 int
 ldp_sync_zebra_send_state_update(struct ldp_igp_sync_if_state *state)
 {
-	debug_evt_ldp_sync("%s: name=%s, ifindex=%d, sync_start=%d",
-		__func__, state->name, state->ifindex, state->sync_start);
-
         return zclient_send_opaque(zclient, LDP_IGP_SYNC_IF_STATE_UPDATE,
 		(const uint8_t *) state, sizeof(*state));
 }
