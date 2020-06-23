4566981abec057fa400be74f803a6334eb04fee1
Karen Schoener <karen@volta.io>
2020-06-23 10:55:04 -0400

    Remove iface-announce event from ldp

--- ldpd/interface.c
+++ ldpd/interface.c
@@ -639,7 +639,6 @@ const char * const ldp_sync_event_names[] = {
 	"IFACE SYNC START (ADJ NEW)",
 	"IFACE SYNC START (SESSION CLOSE)",
 	"IFACE SYNC START (CONFIG LDP ON)",
-	"IFACE ANNOUNCE", // TODO REMOVE ME
 	"IFACE SHUTDOWN",
 	"N/A"
 };
@@ -650,7 +649,6 @@ const char * const ldp_sync_action_names[] = {
 	"LDP START SYNC",
 	"LDP COMPLETE SYNC",
 	"CONFIG LDP OFF",
-	"IFACE ANNOUNCE", // TODO REMOVE ME
 	"IFACE SHUTDOWN",
 	"N/A"
 };
@@ -1003,7 +1001,6 @@ ldp_sync_fsm(struct iface *iface, enum ldp_sync_event event)
 	case LDP_SYNC_ACT_IFACE_SHUTDOWN:
 		ldp_sync_fsm_init(iface, iface->ldp_sync.state);
 		break;
-	case LDP_SYNC_ACT_IFACE_ANNOUNCE: // TODO REMOVE ME?
 	case LDP_SYNC_ACT_NOTHING:
 		/* do nothing */
 		break;
